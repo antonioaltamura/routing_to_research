20e826600da472838d2ea77b73cb144392245de7
antonioaltamura <alfredopacino@alice.it>
2018-07-09 23:26:26 +0200

    unused script removed

--- models_old_QueryBuiler_without_params/index.js
+++
@@ -1,7 +0,0 @@
-exports.Node = require('./Graph');
-exports.Author = require('./Author');
-exports.Book = require('./Book');
-exports.Journal = require('./Journal');
-exports.Paper = require('./Paper');
-exports.Topic = require('./Topic');
-exports.Graph = require('./Graph');
